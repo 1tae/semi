91c25aa067098d1fc56d05b8b577c2c04b77f717
SERYE <SERYE@SERYE>
2020-01-30 18:30:18 +0900

    intranet resource 파일들

---
+++ semi_project/web/resource/js/intranetForm.js
@@ -0,0 +1,16 @@
+function formCategory(){
+    var form = document.getElementById("formCategory");
+    var content = document.getElementById("formContent");
+
+    if(form.value = "지출결의서") {
+        content.innerHTML  = '지출결의서 입니다';
+    } else if(form.value = "휴가신청서") {
+        content.innerHTML = "휴가신청서"
+    }
+
+
+
+
+
+
+}
